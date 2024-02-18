abb50fc0259746d0a6f9e14d62fa88dfb7411b42
Cho-Byeong-Gyu <160321523+Cho-Byeong-Gyu@users.noreply.github.com>
2024-02-18 19:01:09 +0900

    Add files via upload

---
+++ printf 예제.c
@@ -0,0 +1,15 @@
+/*
+ 제 목: print 함수 예제
+ 날짜 : 2024.02.18
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello everybody \n");
+    printf("1234 \n");
+    printf("10 20 \n");
+    return 0;
+
+}
